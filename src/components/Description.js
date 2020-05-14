38c03d56f7ac61a9f428b5d4777a2a6d67cf9372
sujanth21 <sujanth1987@gmail.com>
2020-05-14 12:15:58 +1000

    changed font size

--- src/components/Description.js
+++ src/components/Description.js
@@ -5,19 +5,19 @@ const Description = () => {
     <div className='Description'>
       <div>
         <h3>About Me</h3>
-        <p style={{ fontSize: "1.25rem" }}>
-          A multi-skilled application developer with a proven track record of
-          achieving results in highly competitive environments. Demonstrated
-          ability to innovate, design and develop scalable applications using
-          best practices. Have a blend of multi-disciplinary and cross
-          functional skills in software engineering, system analysis and project
-          management backed by a solid academic track record and experience
-          gained in relevant fields. Easy going by nature, committed to results
-          and possesses the ability to communicate professionally and
+        <p style={{ fontSize: "1.15rem" }}>
+          &nbsp; A multi-skilled application developer with a proven track
+          record of achieving results in highly competitive environments.
+          Demonstrated ability to innovate, design and develop scalable
+          applications using best practices. Have a blend of multi-disciplinary
+          and cross functional skills in software engineering, system analysis
+          and project management backed by a solid academic track record and
+          experience gained in relevant fields. Easy going by nature, committed
+          to results and possesses the ability to communicate professionally and
           effectively at all levels.
         </p>
-        <p style={{ fontSize: "1.25rem" }}>
-          Interested in software development and analysis areas. The key
+        <p style={{ fontSize: "1.15rem" }}>
+          &nbsp; Interested in software development and analysis areas. The key
           qualities are a passion for problem-solving and providing appropriate
           and effective technical solutions. A proven team player and highly
           dedicated and strongly driven individual, able to prioritise, plan and
